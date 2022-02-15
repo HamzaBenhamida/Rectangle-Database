9089af35b32c74be191dc23aac27e54753817836
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2022-02-15 10:42:02 -0800

    adding rectangle from post using server

--- index.js
+++ index.js
@@ -3,19 +3,56 @@ const { appendFile } = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  //connectionString: process.env.DATABASE_URL
+  connectionString: `postgres://postgres:root@localhost/rectangles`
+})
+
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index'));
 app.get('/database', (req,res) => {
   var data = { results: [1,2,3,4,5]}
   res.render('pages/rect_db', data);
 });
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// ADD RECTANGLE: send data of the form to database and update table in index.ejs
+app.post('/addrectangle', (req,res) => {
+  console.log(req.body)
+  var uid = req.body.input-uid;
+  var name = req.body.input-name;
+  var color = req.body.input-color;
+  var width = req.body.input-width;
+  var height = req.body.input-height;
+
+  var results = { "uid": uid, 
+                  "name": name, 
+                  "color":color, 
+                  "width":width, 
+                  "height":height }
+
+  // ADD RECTANGLE TO THE DATABASE
+  var addRectangleQuery = `insert into rectangles values('${uid}', '${name}', '${color}', '${width}', ''${height}) `;
+  pool.query(addRectangleQuery, (error,result) => {
+    if(error)
+      res.end(error);
+
+    res.render('pages/index', results);
+  })
+
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
   // .get('/', async (req,res)=> {
   //   try {
--- public/main.js
+++ public/main.js
@@ -1,17 +1,66 @@
-let numRectangle = 1;
+// let numRectangle = 1;
 
 	
-function update(e) {
-	print('ok update');
-}
+// function update(e) {
+// 	print('ok update');
+// }
 
-function delete_rec(e) {
-	print('ok delete');
-}
+// function delete_rec(e) {
+// 	print('ok delete');
+// }
 
-// ADD RECTANGLE
+// // ADD RECTANGLE
+// let addBtn = document.getElementById("add");
+// let table = document.getElementById("table");
+
+// addBtn.addEventListener('click', () => {
+// 	numRectangle += 1;
+	
+// 	//insert new row
+// 	let newRow = table.insertRow(numRectangle);
+// 	newRow.setAttribute('class', 'rectangles');
+
+// 	//insert Name Cell
+// 	let cell0 = newRow.insertCell(0);
+// 	let text = "name" + numRectangle;
+// 	let name = document.createTextNode(text);
+// 	cell0.appendChild(name);
+
+// 	//insert color cell
+// 	let cell1 = newRow.insertCell(1);
+// 	// get color from input
+// 	text = 'color'
+// 	let color = document.createTextNode(text);
+// 	cell1.appendChild(color);
+
+// 	//insert delete cell
+// 	let cell2 = newRow.insertCell(2);
+// 	let delete_button = document.createElement("button");
+// 	let span = document.createTextNode("DELETE");
+// 	delete_button.setAttribute('class', 'delete');
+// 	let t = 'delete'+ numRectangle;
+// 	delete_button.setAttribute('id', t)
+// 	delete_button.addEventListener('onClick', delete_rec)   //add onclick event listner
+// 	delete_button.appendChild(span);
+// 	cell2.appendChild(delete_button);
+
+// 	//insert update cell
+// 	let cell3 = newRow.insertCell(3);
+// 	let update_button = document.createElement("button");
+// 	let span2 = document.createTextNode("UPDATE");
+// 	update_button.setAttribute('class', 'update');
+// 	t = 'update'+numRectangle;
+// 	update_button.setAttribute('id', t)
+// 	update_button.addEventListener('onClick', update)   //add onclick event listner
+// 	update_button.appendChild(span2);
+// 	cell3.appendChild(update_button);
+		
+// });
+
+// ADD ROW HTML 
 let addBtn = document.getElementById("add");
 let table = document.getElementById("table");
+let numRectangle = 1;
 
 addBtn.addEventListener('click', () => {
 	numRectangle += 1;
@@ -20,18 +69,27 @@ addBtn.addEventListener('click', () => {
 	let newRow = table.insertRow(numRectangle);
 	newRow.setAttribute('class', 'rectangles');
 
+  //insert uid Cell
+  let cell = newRow.insertCell(0);
+	let text = results.uid;
+	let uidNode = document.createTextNode(text);
+	cell.appendChild(uidNode);
+
 	//insert Name Cell
 	let cell0 = newRow.insertCell(0);
-	let text = 'n;
-	let name = document.createTextNode(text);
-	cell0.appendChild(name);
+	text = results.name;
+	let nameNode = document.createTextNode(text);
+  let link = document.createElement("a");
+  link.setAttribute('href', "/rect_db.ejs");
+	cell0.appendChild(nameNode);
+  cell0.appendChild(link);
 
 	//insert color cell
 	let cell1 = newRow.insertCell(1);
 	// get color from input
-	text = 'color'
-	let color = document.createTextNode(text);
-	cell1.appendChild(color);
+	text = results.color;
+	let colorNode = document.createTextNode(text);
+	cell1.appendChild(colorNode);
 
 	//insert delete cell
 	let cell2 = newRow.insertCell(2);
@@ -43,17 +101,5 @@ addBtn.addEventListener('click', () => {
 	delete_button.addEventListener('onClick', delete_rec)   //add onclick event listner
 	delete_button.appendChild(span);
 	cell2.appendChild(delete_button);
-
-	//insert update cell
-	let cell3 = newRow.insertCell(3);
-	let update_button = document.createElement("button");
-	let span2 = document.createTextNode("UPDATE");
-	update_button.setAttribute('class', 'update');
-	t = 'update'+numRectangle;
-	update_button.setAttribute('id', t)
-	update_button.addEventListener('onClick', update)   //add onclick event listner
-	update_button.appendChild(span2);
-	cell3.appendChild(update_button);
 		
 });
-
