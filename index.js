87782e714da378bad298c866fe6452e39be1145b
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2022-02-16 21:08:23 -0800

    html ready

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Pool } = require('pg');
 var pool;
 pool = new Pool({
   //connectionString: process.env.DATABASE_URL
-  connectionString: `postgres://postgres:root@localhost/rectangles`
+  connectionString: `postgres://postgres:Benkendo7210@localhost/rectangles`
 })
 
 var app = express();
@@ -21,35 +21,118 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'));
+
+//VIEW ALL RECTANGLES in MAIN PAGE (rec_db.ejs)
 app.get('/database', (req,res) => {
-  var data = { results: [1,2,3,4,5]}
-  res.render('pages/rect_db', data);
+
+  var viewAllQuery = `SELECT * FROM rec`;
+  pool.query(viewAllQuery, (error,result) => {
+    if(error)
+      console.log(error);
+      //res.end(error);
+
+    console.log(result);
+    const rows = result.rows
+    
+    res.render('pages/rec_db', {rows});
+  });
+});
+
+//UPDATE values in display_rec.ejs
+app.get('/display', (req,res) => {
+
+  var selectRecQuery = `SELECT * FROM rec where `;
+  pool.query(viewAllQuery, (error,result) => {
+    if(error)
+      console.log(error);
+      //res.end(error);
+
+    console.log(result);
+    const rows = result.rows
+    
+    res.render('pages/display_rec', {rows});
+  });
 });
 
 // ADD RECTANGLE: send data of the form to database and update table in index.ejs
 app.post('/addrectangle', (req,res) => {
-  console.log(req.body)
-  var uid = req.body.input-uid;
-  var name = req.body.input-name;
-  var color = req.body.input-color;
-  var width = req.body.input-width;
-  var height = req.body.input-height;
+  let ending = '...'
 
-  var results = { "uid": uid, 
-                  "name": name, 
-                  "color":color, 
-                  "width":width, 
-                  "height":height }
+  //checking for errors in format of inputs
+  var name = req.body.input-name;
+  if(name.lenght > 20){
+    name = name.substring(0, 20 - ending.length) + ending;
+  }
+  var color = req.body.input-color;
+  if(name.lenght > 20){
+    color = color.substring(0, 20 - ending.length) + ending;
+  }
+  var width = req.body.input-width;
+  if (width<0){
+    width = Math.abs(width);
+  }
+  var height = req.body.input-height;
+  if (height<0){
+     height = Math.abs(height);
+  }
 
   // ADD RECTANGLE TO THE DATABASE
-  var addRectangleQuery = `insert into rectangles values('${uid}', '${name}', '${color}', '${width}', ''${height}) `;
+  var addRectangleQuery = `INSERT INTO rec values (DEFAULT, '${name}', ${width}, ${height},'${color}') `;
   pool.query(addRectangleQuery, (error,result) => {
     if(error)
-      res.end(error);
+      console.log(error);
 
-    res.render('pages/index', results);
+    res.redirect('/database');
   })
 
+});
+
+// UPDATE RECTANGLE: send data of the form to sql and update values in .ejs
+app.post('/updaterectangle', (req,res) => {
+  let ending = '...'
+
+  //checking for errors in format of inputs
+  var name = req.body.input-name;
+  if(name.lenght > 20){
+    name = name.substring(0, 20 - ending.length) + ending;
+  }
+  var color = req.body.input-color;
+  if(name.lenght > 20){
+    color = color.substring(0, 20 - ending.length) + ending;
+  }
+  var width = req.body.input-width;
+  if (width<0){
+    width = Math.abs(width);
+  }
+  var height = req.body.input-height;
+  if (height<0){
+     height = Math.abs(height);
+  }
+
+  // ADD RECTANGLE TO THE DATABASE
+  var updateRectangleQuery = `INSERT INTO rec values (DEFAULT, '${name}', ${width}, ${height},'${color}') `;
+  pool.query(updateRectangleQuery, (error,result) => {
+    if(error)
+      console.log(error);
+
+    res.redirect('/display');
+  })
+
+});
+
+// DELETE rectangle
+app.post('/deleterectangle', (req,res) => {
+  console.log(req);
+  let uid = req.body.id;
+  console.log(uid);
+
+  let deleteRectangleQuery = `DELETE from rec WHERE uid=${uid}`;
+  pool.query(deleteRectangleQuery, (error,result) => {
+    if(error)
+      console.log(error);
+
+    res.redirect('/database');
+  })
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
